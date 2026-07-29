3518683b1d802e6c36179ba448c5818cc4bc512b
Bhawandeep Singh <bhawandeeps@protonmail.com>
2026-07-29 18:57:14 +0530

    added windows compatability

--- prompt.c
+++ prompt.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef _WIN32
+#include <string.h>
+
+//fake functions for windows
+static char buffer[]
+
+char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy)-1] = '\0';
+    return cpy;
+}
+
+void add_history(char* unused) []
+
+#else
 #include <editline/readline.h>
+
+#endif
 
 int main(int argc, char *argv[])
 {
