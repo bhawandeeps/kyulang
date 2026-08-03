192b3593ee4478cd90ecf6c9cb4d22f0b64109de
Bhawandeep Singh <bhawandeeps@protonmail.com>
2026-08-03 18:43:38 +0530

    added evaluation

--- evaluation.c
+++ evaluation.c
@@ -25,6 +25,10 @@ void add_history(char* unused) {}
 
 #endif
 
+long eval(mpc_ast_t* t);
+
+long eval_op(long x, char* op, long y);
+
 int main(int argc, char *argv[])
 {
     
@@ -53,6 +57,8 @@ int main(int argc, char *argv[])
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Kyulang, &r)) {
             mpc_ast_print(r.output);
+            long result = eval(r.output);
+            printf("I uh- uh- I think it is %li\n", result);
             mpc_ast_delete(r.output);
         } else {
             mpc_err_print(r.error);
@@ -64,5 +70,30 @@ int main(int argc, char *argv[])
 
     mpc_cleanup(4, Number, Operator, Expr, Kyulang);
 
+    return 0;
+}
+
+long eval(mpc_ast_t* t) {
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+
+    char* op = t->children[1]->contents;
+
+    long x = eval(t->children[2]);
+
+    int i = 3;
+    while(strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+    return x;
+}
+
+long eval_op(long x, char* op, long y) {
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
     return 0;
 }
